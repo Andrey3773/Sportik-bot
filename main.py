d86c65906e15eb0325e00eeb7b82a20fd963f78e
Andrey3773 <andrejtimofeev311@gmail.com>
2026-03-13 14:07:13 +0300

    psycopg2 to asyncpg

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import asyncio
+import asyncpg
+
+from asyncpg.pool import Pool
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
@@ -14,11 +17,22 @@ from config.config_env import load_config
 async def main():
     config = load_config('.env')
     storage = MemoryStorage()
+
+    db_pool: Pool = await asyncpg.create_pool( # noqa
+        user=config.db.db_user,
+        password=config.db.db_password,
+        database=config.db.db_name,
+        host=config.db.db_host,
+        min_size=1,
+        max_size=10,
+    )
+
     bot = Bot(
         token=config.bot.token,
         default=DefaultBotProperties(parse_mode=ParseMode.HTML)
     )
     dp = Dispatcher(storage=storage)
+    dp["db_pool"] = db_pool
 
 
     routers = [base.router, insert.router, rest.router, output.router]
@@ -31,7 +45,9 @@ async def main():
     try:
         await dp.start_polling(bot)
     finally:
+        await db_pool.close()
         await bot.session.close()
 
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
